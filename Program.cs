6dff26e9eedbf941918a72beaf354652cc1a5659
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-12-31 04:33:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json.Nodes;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using KanyeAndRon;
+
+namespace KanyeAndRon
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 5;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("--------------");
+                QuoteGenerator.KanyeQuote();
+                Console.WriteLine("--------------");
+                QuoteGenerator.RonQuote();
+            }
+        }
+    }
+}
